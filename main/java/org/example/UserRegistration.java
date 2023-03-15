050351ec74429e552e1e19c79f3c1fa1340a4d48
akashkbarik <akashkbarik12@gmail.com>
2023-03-16 00:50:54 +0530

    Added: email code

--- main/java/org/example/UserRegistration.java
+++ main/java/org/example/UserRegistration.java
@@ -37,11 +37,27 @@ public class UserRegistration {
             System.out.println(inputStr + " is invalid name.Kindly input the right one.");
         }
     }
+    public void email(){
+        String mailPattern = "^abc(.+)[A-Za-z0-9]{3}+(@+)bl+(.+)[co]*(.[A-Za-z]{2})$";
+        Pattern regex = Pattern.compile(mailPattern);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the email ID : ");
+        String inputStr = sc.nextLine();
+        Matcher emailIDMatcher = regex.matcher(inputStr);
+        sc.close();
+        if (emailIDMatcher.matches()) {
+            System.out.println(inputStr + " is valid mail.");
+        } else {
+            System.out.println(inputStr + " is invalid mail address.Kindly input the right one.");
+        }
+    }
 
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.firstname();
         userRegistration.lastname();
+        userRegistration.email();
 
 
     }
